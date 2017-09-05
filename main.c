2bd55ed4f8b5ec1ada369870025a939b99a66ef9
zh.c <584039362@qq.com>
2017-09-05 22:54:09 +0800

    An in-memory, append only, Single-Table database

--- main.c
+++ main.c
@@ -31,9 +31,10 @@ typedef enum MetaCommandResult_t MetaCommandResult;
 
 enum PrepareResult_t{
     PREPARE_SUCCESS,
+    PREPARE_SYNTAX_ERROR,
     PREPARE_UNRECOGNIZED_STATEMENTS
 };
-typedef enum  PrepareResult_t PrepareResult;
+typedef enum PrepareResult_t PrepareResult;
 
 enum StatementType_t {
     STATEMENT_INSERT,
@@ -41,11 +42,83 @@ enum StatementType_t {
 };
 typedef enum StatementType_t StatementType;
 
+
+enum ExecuteResult_t{
+    EXECUTE_SUCCESS,
+    EXECUTE_TABLE_FULL
+};
+typedef enum ExecuteResult_t ExecuteResult;
+
+const uint32_t COLUMN_USERNAME_SIZE = 32;
+const uint32_t COLUMN_EMAIL_SIZE = 255;
+
+struct Row_t {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+};
+typedef struct Row_t Row;
+
 struct Statement_t{
     StatementType type;
+    Row row_to_insert; //only used by insert statement
 };
 typedef struct Statement_t Statement;
 
+#define size_of_attribute(Struct, attribute) sizeof(((Struct*)0)->attribute)
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+const uint32_t PAGE_SIZE = 4096;
+const uint32_t TABLE_MAX_PAGES = 100;
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+struct Table_t{
+    void *Pages[TABLE_MAX_PAGES];
+    uint32_t num_rows;
+};
+typedef struct Table_t Table;
+
+void print_row(Row* row){
+    printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
+
+void serialize_row(Row* source, void* destination){
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void* source, Row* destination){
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+
+}
+
+void* row_slot(Table* table, uint32_t row_num){
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void *page = table->Pages[page_num];
+    if (!page){
+        page = table->Pages[page_num] = malloc(PAGE_SIZE);
+    }
+
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+    return page + byte_offset;
+}
+
+Table* new_table(){
+    Table* table = malloc(sizeof(table));
+    table->num_rows = 0;
+    return table;
+}
 MetaCommandResult do_meta_command(InputBuffer *inputBuffer){
     if(strcmp(inputBuffer->buffer, ".exit") == 0){
         exit(EXIT_SUCCESS);
@@ -54,9 +127,15 @@ MetaCommandResult do_meta_command(InputBuffer *inputBuffer){
     }
 }
 
+
+//our sql compiler
 PrepareResult prepare_statement(InputBuffer* inputBuffer, Statement* statement){
     if(strncmp(inputBuffer->buffer, "insert", 6) == 0){
         statement->type = STATEMENT_INSERT;
+        int args_assigned = sscanf(inputBuffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
+        if (args_assigned < 3){
+           return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if (strcmp(inputBuffer->buffer, "select") == 0) {
@@ -66,14 +145,34 @@ PrepareResult prepare_statement(InputBuffer* inputBuffer, Statement* statement){
     return PREPARE_UNRECOGNIZED_STATEMENTS;
 }
 
-void execute_statement(Statement* statement){
+ExecuteResult execute_insert(Statement* statement, Table* table){
+    if(table->num_rows >= TABLE_MAX_ROWS){
+        return EXECUTE_TABLE_FULL;
+    }
+
+    Row *row_to_insert = &(statement->row_to_insert);
+    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    table->num_rows += 1;
+    return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_select(Statement* statement, Table* table){
+    Row row;
+    for(uint32_t i = 0; i < table->num_rows; i++){
+        deserialize_row(row_slot(table, i), &row);
+        print_row(&row);
+    }
+    return EXECUTE_SUCCESS;
+}
+
+
+//our virtual machine
+ExecuteResult execute_Statement(Statement* statement, Table* table){
     switch (statement->type){
         case STATEMENT_INSERT:
-            printf("This is where we would do an insert.\n");
-            break;
+            return execute_insert(statement, table);
         case STATEMENT_SELECT:
-            printf("This is where we would do an select.\n");
-            break;
+            return execute_select(statement, table);
     }
 }
 
@@ -95,6 +194,7 @@ void read_input(InputBuffer* inputBuffer){
 
 int main(int argc, char** args) {
     InputBuffer* inputBuffer = new_input_buffer();
+    Table* table = new_table();
     while(true){
         print_prompt();
         read_input(inputBuffer);
@@ -113,12 +213,21 @@ int main(int argc, char** args) {
         switch (prepare_statement(inputBuffer, &statement)){
             case PREPARE_SUCCESS:
                 break;
+            case PREPARE_SYNTAX_ERROR:
+                printf("Syntax error. Could not parse statement.\n");
+                continue;
             case PREPARE_UNRECOGNIZED_STATEMENTS:
                 printf("Unrecognized keyword at start of '%s'.\n", inputBuffer->buffer);
                 continue;
         }
-        execute_statement(&statement);
-        printf("Execcted!\n");
+        switch (execute_Statement(&statement, table)){
+            case EXECUTE_SUCCESS:
+                printf("Executed.\n");
+                break;
+            case EXECUTE_TABLE_FULL:
+                printf("Error: Table full.\n");
+                break;
+        }
     }
 }
 
